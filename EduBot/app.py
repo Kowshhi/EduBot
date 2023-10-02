fcb8616eafd317d2de4f1b83df5c4fa07ceb8fd2
Kowshhi <111121007+Kowshhi@users.noreply.github.com>
2023-10-02 21:59:32 +0530

    Additional Features added
    Filtration of Content is compromised.

---
+++ EduBot/app.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request, send_from_directory, send_file
+from utils.prac import *
+from utils.knowledge_graph import generate_knowledge_graph
+from utils.ques_generation.generate_questions import get_questions
+from utils.domain_summarizer import get_domain_summary
+import os
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
+@app.route("/bot")
+def bot():
+    os.system("py CW1.py")
+    return render_template('home.html')
+
+
+
+
+
+# ROUTES FOR SUMMARY GENERATION
+
+@app.route("/revise")
+def summary_page():
+    return render_template('revise.html')
+
+@app.route("/generate_key", methods=['GET', 'POST'])
+def generate_summary():
+    if request.method=='POST':
+        text_input = request.form['original_text']
+        result = ""
+
+        if str(text_input).strip()=="":
+            return render_template('key_points.html', result="Unable to generate summary :(", original=text_input)
+
+
+        choice = request.form['purpose']
+        if choice=="gen":
+            sentences =  clean_text(text_input)
+            text_data = cnt_in_sent(sentences)
+
+            freq_list = freq_dict(sentences)
+            tf_scores = calc_TF(text_data, freq_list)
+            idf_scores = calc_IDF(text_data, freq_list)
+
+            tfidf_scores = calc_TFIDF(tf_scores, idf_scores)
+
+            sent_data = sent_scores(tfidf_scores, sentences, text_data)
+            result = summary(sent_data)
+        else:
+            result = get_domain_summary(text_input,choice)
+
+        return render_template('key_points.html', result=result, original=text_input)
+
+# ROUTES FOR MIND MAP GENERATION
+@app.route("/map", methods=['GET','POST'])
+def map():
+    if request.method=='POST':
+        text = request.form['kg_text']
+        image_title = generate_knowledge_graph(text)
+        if image_title==False:
+            return render_template('mindmap.html')
+        else:
+            return render_template('mindmap.html', image_title=image_title)
+    else:
+        return render_template('map.html')
+
+# view/download mindmap
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def view(filename):
+    image_dir = os.path.join(app.root_path, "/static/media/")
+    return send_from_directory(directory=image_dir, filename=filename.split("/")[-1],as_attachment=True,attachment_filename=filename.split("/")[-1]) 
+
+
+# ROUTES FOR QUESTION GENERATION
+
+@app.route("/question")
+def question():
+    return render_template('question.html')  
+ 
+
+@app.route("/generate_questions", methods=['GET', 'POST'])
+def generate_questions():
+    text_input = request.form['original_text']
+    question = get_questions(text_input)
+    if question == []:
+        return render_template('question_gen.html', original=text_input)
+    random.shuffle(question)
+    return render_template('question_gen.html', original=text_input, result=question)
+  
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
